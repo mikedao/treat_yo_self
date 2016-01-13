bd1b9a63ef090a2f692b0b07b985739ff42d827e
Michael Dao <michael.dao@gmail.com>
2016-01-13 11:45:27 -0700

    Added most expensive test

--- test/integration/root_page_lists_items_test.rb
+++ test/integration/root_page_lists_items_test.rb
@@ -10,4 +10,22 @@ class RootPageListsItemsTest < ActionDispatch::IntegrationTest
     assert page.has_content?("5")
   end
 
+  test "root page has a three most expensive section" do
+    item_1 = Item.create(name: "Power Wheels", price: 10)
+    item_2 = Item.create(name: "Pound Puppies" , price: 9)
+    item_3 = Item.create(name: "Thundercats", price: 8)
+    item_3 = Item.create(name: "Lincoln Logs", price: 7)
+
+
+    visit root_path
+
+    within("#top-three") do
+      assert page.has_content?("Power Wheels")
+      assert page.has_content?("Pound Puppies")
+      assert page.has_content?("Thundercats")
+      refute page.has_content?("Lincoln Logs")
+    end
+
+  end
+
 end
