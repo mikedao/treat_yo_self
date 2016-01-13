17a4d9f9224c83a88cf05f0372a257920d954b2d
Michael Dao <michael.dao@gmail.com>
2016-01-12 20:44:35 -0700

    base functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113031122) do
+ActiveRecord::Schema.define(version: 20160113031243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160113031122) do
     t.string   "name"
     t.integer  "price"
     t.boolean  "do_i_deserve_this"
+    t.boolean  "cool"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+class Seed
+
+  def initialize
+    generate_items
+  end
+
+  def generate_items
+    Item.create(name: "Light Bright", price: 10)
+    Item.create(name: "Blocks", price: 20)
+    Item.create(name: "Polly Pocket", price: 15)
+    Item.create(name: "Lincoln Logs", price: 5)
+    Item.create(name: "Hot Wheels", price: 5)
+  end
+end
+
+Seed.new
