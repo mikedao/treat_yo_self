9f8c51e37ffd0cfe15f695bd9334d19a5242ca36
Michael Dao <michael.dao@gmail.com>
2016-01-13 19:01:05 -0700

    Completed Callbacks

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ActiveRecord::Base
+  before_create :make_cool,
+                :make_deserved
+
+  def make_cool
+    self.cool = true
+  end
+
+  def make_deserved
+    self.do_i_deserve_this = true
+  end
 end
