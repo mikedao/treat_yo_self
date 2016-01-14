49193a26d90a044fde46ba3d848be2e3c22397e0
Michael Dao <michael.dao@gmail.com>
2016-01-13 19:03:01 -0700

    Added top three CSS

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,16 +1,4 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  test "an item is cool by default" do
-    item = Item.create(name: "Light Bright", price: 30)
-
-    assert item.cool
-  end
-
-  test "you deserve this item by default" do
-    item = Item.create(name: "Light Bright", price: 30)
-
-    assert item.do_i_deserve_this
-  end
-
 end
